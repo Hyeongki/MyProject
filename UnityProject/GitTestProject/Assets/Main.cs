8e75d21ef44140f0f592e3abbfce138891660b5e
Hyeongki <dark7wizard@ikinagames.com>
2018-08-23 17:07:46 +0900

    add unity project

---
+++ UnityProject/GitTestProject/Assets/Main.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 100, 50), "test"))
+        {
+            Debug.Log("Teeeest");
+        }
+    }
+}
